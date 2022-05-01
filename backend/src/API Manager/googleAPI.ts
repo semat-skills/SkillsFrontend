a878fbe1a5c721514e176a969b35a420d638aaf1
Semat Igbaria <simati@academy-skills.com>
2022-05-01 10:03:39 +0300

    check

--- backend/src/API Manager/googleAPI.ts
+++ backend/src/API Manager/googleAPI.ts
@@ -1,8 +1,5 @@
 import axios, { AxiosResponse } from "axios";
 
-const query =
-  "mutation ($myItemName: String!, $columnVals: JSON!) { create_item (board_id:2584907448, item_name:$myItemName, column_values:$columnVals) { id } }";
-
 export const searchImg = async (searchFor: string) => {
   const search: AxiosResponse = await axios.get(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API}&cx=${process.env.CX}&q=${searchFor}`
